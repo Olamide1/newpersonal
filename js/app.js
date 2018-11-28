a2398a95eac25d529b2ca39729456d6a2ddba2a7
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2018-11-28 10:43:15 +0000

    Changes made to the email sender

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function show () {
     var e = document.getElementById("subject");
     var subject = e.options [e.selectedIndex] .value;
     var email = document.getElementById("email").value;
-    var message =  'My name is ' + name + '' +  document.getElementById("message").value + '' + email;
+    var message =  'My name is ' + name + '' +  document.getElementById("message").value + ' My email is: ' + email;
     var myEmail = "olamideakomolafe1234@gmail.com";
     var host = "smtp.gmail.com";
     var username = "olamideakomolafe1234@gmail.com";
@@ -17,7 +17,9 @@ function show () {
 
     mySubject = "Hi " + name  + " Thanks For Reaching Out";
     myMessage = "Thank you for reaching out, I would get back to you as soon as I can.";
-    Email.send(myEmail, email, mySubject, myMessage, host, username, password);
+    Email.send(myEmail, email, mySubject, myMessage, host, username, password, function(message){
+        console.log(message);
+    });
     clear();
 }
 
